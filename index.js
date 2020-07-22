bcb0f4c9a6d839c47c7adc1697764d4eeed4a03e
Kateryna Shylovska <murkotik@Katerynas-MacBook.local>
2020-07-22 09:12:16 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var numbers = 0;
+for (numbers = 0; numbers<100; numbers++){
+  numbers;
+}
+// if (numbers === 9){
+//   numbers =  'Cat';
+// }
+// else if (numbers === 25){
+//   numbers = 'Dog'
+// }
+// else if (numbers === 15){
+//   numbers = 'CatDog'
+// }
+//
+// console.log(numbers);
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {MatIconModule, MatInputModule, MatMenuModule, MatToolbarModule} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/typings/table';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatInputModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
